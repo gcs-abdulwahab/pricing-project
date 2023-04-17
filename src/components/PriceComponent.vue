a3c04a542c65931bafd3dd140ddb42c280cf5f73
gcs-abdulwahab <100867378+gcs-abdulwahab@users.noreply.github.com>
2023-04-17 09:57:39 +0500

    pricecomponent fontawesome

--- src/components/PriceComponent.vue
+++ src/components/PriceComponent.vue
@@ -1,6 +1,20 @@
 <template>
-    <div class="bg-green-400" >
-        ds
-        
+    <div class="bg-white flex flex-col space-y-2 p-2 max-w-sm rounded-2xl shadow-2xl" >
+        <h1 class="uppercase text-2xl font-bold "> basic </h1>
+        <h2 class="font-semibold" > $19.99 / Year</h2>
+        <button type="button" class="font-bold text-blue-700 bg-gray-300 
+        hover:text-white
+        hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 uppercase">buy</button>
+        <hr/>
+        <ul class="flex flex-col bg-white p-5 list-none items-start">
+            
+            <li class=""><i class="px-2 fas fa-check-circle text-green-500"></i> 500 GB Storage </li>
+            <li class=""><i class="px-2 fas fa-check-circle text-green-500"></i> 2 Users Allowed </li>
+            <li class=""><i class="px-2 fas fa-check-circle text-green-500"></i> Send up to 3 GB </li>
+            <li class=""><i class="px-2 fa-regular fa-circle-xmark text-red-700"></i> Send up to 10 GB </li>
+
+            
+
+        </ul>        
     </div>
 </template>
